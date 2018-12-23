7b58b0a3e44450eb5ff81c4d1c677a29e11785eb
Andrés Soto <andres.soto.h@outlook.com>
2018-12-23 15:21:37 -0500

    add method for creating products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,16 +1,19 @@
 class Api::V1::ProductsController < ApplicationController
 
+  protect_from_forgery with: :null_session
+
   def index
     @products = Product.all
       render json: @products
   end
 
-  def new
-    @product = Product.new
-  end
-
   def create
-    @product = Product.create(product_params)
+    product = Product.new(product_params)
+    if product.save
+      render json: product, status: 201
+    else
+      render json: { errors: product.errors }, status: 422
+    end
   end
 
   def edit
