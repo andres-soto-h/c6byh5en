25b0546ef615e70319885f90521e7a1e121559e7
Andrés Soto <andres.soto.h@outlook.com>
2018-12-24 18:55:16 -0500

    correct @product error

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -22,7 +22,7 @@ class Api::V1::ProductsController < ApplicationController
     if @product.update(product_params)
       render json: @product, status: 200
     else
-      render json: { errors: product.errors }, status: 422
+      render json: { errors: @product.errors }, status: 422
     end
   end
 
